da54f80bde9a19dd70c7c1a3b33aecb667147475
DanieldeJongVeldstra <danieldejongg@hotmail.com>
2023-10-16 19:52:51 +0200

    Add Jpa repositories for reproduction

--- example/src/main/java/nl/daniel/dejong/Application.java
+++ example/src/main/java/nl/daniel/dejong/Application.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableJpaRepositories
 @ConfigurationPropertiesScan
 public class Application {
     public static void main(String... args) {
